99f6c7dc10d9efa186d1c4f3b6fc0f5760f754c0
Brandon Essler <brandon.essler@gmail.com>
2018-12-17 19:39:38 -0600

    Day 2a Kotlin

---
+++ Kotlin/src/InventoryManagementSystem.kts
@@ -0,0 +1,8 @@
+
+// For each box id in list
+//   For each letter in box id
+//     Add to hashmap where key is letter and value is count
+//   iterate through hashmap
+//     increment 2count for first key with value 2
+//     increment 3count for first key with value 3
+// Multiply 2count by 3count
